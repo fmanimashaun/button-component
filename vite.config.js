48cb0aeeb681b90629ddd047f3f429ec3d08f156
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-08-31 20:22:15 +0100

    update style setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import PurgeCSS from 'vite-plugin-purgecss';
+import path from 'path'; // Add this import
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    PurgeCSS({
+      content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    })
+  ],
+  css: {
+    postcss: './postcss.config.cjs'
+  },
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, 'src/'), // Add this alias configuration
+    }
+  }
+});
